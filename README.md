fd39bff42977582ac97a954ddc9de01f9f77e37c
dmarkin@commercegate.com <dmarkin@commercegate.com>
2018-04-27 10:26:29 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # react-floating-scroll
 Floating Scroll React Component
+Inspired by jQuery Floating Scroll plugin (https://amphiluke.github.io/floating-scroll/)
+
+Used espesially for antd (https://ant.design/) wide and long tables
+
+Usage at the parent component:
+
+```js
+import SteakyScroll from 'components/general/SteakyScroll';
+...
+
+class YourParentComponent extends Component {
+
+constructor(props) {
+    super(props);
+
+    this.contentRef = React.createRef();
+    ...
+    }
+
+render() {
+return (
+<SteakyScroll tableRef={this.tableRef} innerScrollableClass={'ant-table-body'}>
+<Table {...props}
+ref={this.contentRef}>
+</SteakyScroll>);
+}
+}
+```
---
+++ dist/SteakyScroll.js
@@ -0,0 +1,146 @@
+import React, {Fragment, Component} from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import './SteakyScroll.css';
+
+class SteakyScroll extends Component {
+  static propTypes = {
+    contentRef: PropTypes.object.isRequired,
+    innerScrollableClass: PropTypes.string
+  };
+
+  static defaultProps = {
+
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.scrollRef = React.createRef();
+    this.scrollBlockRef = React.createRef();
+
+    this.state = {
+      contentRefEl: ReactDOM.findDOMNode(props.contentRef.current),
+      scrollRefEl: this.scrollRef.current,
+      visible: true,
+      preventSyncCont: false,
+      preventSyncSbar: false
+    };
+  }
+
+  componentDidMount() {
+    const contentRefEl = ReactDOM.findDOMNode(this.props.contentRef.current);
+    const elToScroll = contentRefEl.getElementsByClassName(this.props.innerScrollableClass)[0];
+    const scrollWidth = contentRefEl && elToScroll ? elToScroll.scrollWidth : 0;
+    const scrollRefEl = this.scrollRef.current;
+    const scrollBlockRefEl = this.scrollBlockRef.current;
+    const visible = this.state.visible;
+    const _this = this;
+
+    if (contentRefEl && elToScroll) {
+      elToScroll.onscroll = (e) => {
+        _this.syncSbar(e.target, true);
+      };
+      elToScroll.onfocus = () => {
+        setTimeout(_this.syncSbar.bind(_this, contentRefEl), 0);
+      };
+    }
+
+    if (scrollRefEl) {
+      scrollRefEl.onscroll = (e) => {
+        visible && _this.syncCont(e.target, true);
+      };
+    }
+
+    window.onscroll = this.checkVisibility;
+    window.onresize = this.checkVisibility;
+
+    const contRect = contentRefEl.getBoundingClientRect();
+    const maxVisibleY = window.innerHeight || document.documentElement.clientHeight;
+    const mustHide = ((contRect.bottom <= maxVisibleY) || (contRect.top > maxVisibleY));
+
+    scrollBlockRefEl.style.width = `${scrollWidth}px`;
+
+    this.setState({contentRefEl, scrollRefEl, visible: !mustHide});
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const contentRefEl = ReactDOM.findDOMNode(this.props.contentRef.current);
+    const scrollBlockRefEl = this.scrollBlockRef.current;
+    const elToScroll = contentRefEl.getElementsByClassName(this.props.innerScrollableClass)[0];
+    const scrollWidth = contentRefEl && elToScroll ? elToScroll.scrollWidth : 0;
+    scrollBlockRefEl.style.width = `${scrollWidth}px`;
+  }
+
+  checkVisibility = () => {
+    let mustHide = (parseInt(this.scrollBlockRef.current.style.width) <= this.state.scrollRefEl.offsetWidth);
+
+    if (!mustHide) {
+      let contRect = this.state.contentRefEl.getBoundingClientRect();
+      let maxVisibleY = window.innerHeight || document.documentElement.clientHeight;
+      mustHide = ((contRect.bottom <= maxVisibleY) || (contRect.top > maxVisibleY));
+    }
+    if (this.state.visible === mustHide) {
+      this.setState({visible: !mustHide});
+    }
+  };
+
+  syncSbar = (sender, preventSyncCont) => {
+    if (this.state.preventSyncSbar === true) {
+      this.setState({preventSyncSbar: false});
+      return;
+    }
+    this.setState({preventSyncCont: !!preventSyncCont});
+    this.scrollRef.current.scrollLeft = sender.scrollLeft;
+  };
+
+  syncCont = (sender, preventSyncSbar) => {
+    const contentRefEl = ReactDOM.findDOMNode(this.props.contentRef.current);
+    const elToScroll = contentRefEl.getElementsByClassName(this.props.innerScrollableClass)[0];
+
+    if (this.state.preventSyncCont === true) {
+      this.setState({preventSyncCont: false});
+      return;
+    }
+    this.setState({preventSyncSbar: !!preventSyncSbar});
+    if (contentRefEl && elToScroll) {
+      elToScroll.scrollLeft = sender.scrollLeft;
+    }
+  };
+
+  componentWillUnmount() {
+    window.onscroll = null;
+    window.onresize = null;
+
+    const contentRefEl = ReactDOM.findDOMNode(this.props.contentRef.current);
+    const elToScroll = contentRefEl.getElementsByClassName(this.props.innerScrollableClass)[0];
+    const scrollRefEl = this.scrollRef.current;
+
+    if (contentRefEl && elToScroll) {
+      elToScroll.onscroll = null;
+      elToScroll.onfocus = null;
+    }
+
+    if (scrollRefEl) {
+      scrollRefEl.onscroll = null;
+    }
+  }
+
+  render() {
+    const {visible, contentRefEl} = this.state;
+
+    return (
+        <Fragment>
+          {this.props.children}
+          <div className={`fl-scrolls${!visible ? ' fl-scrolls-hidden' : ''}`}
+               style={{width: `${contentRefEl ? contentRefEl.offsetWidth : 0}px`,
+                 left: `${contentRefEl ? contentRefEl.getBoundingClientRect().left : 0}px`}}
+               ref={this.scrollRef}>
+            <div ref={this.scrollBlockRef}>
+            </div>
+          </div>
+        </Fragment>);
+  }
+}
+
+export default SteakyScroll;
